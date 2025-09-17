b79ecc4f03ef0ea44374adc8f4e0699062cb35ff
hemanthcs34 <cshemanthcs@gmail.com>
2025-09-17 11:37:24 +0530

    changes to product

--- src/main/java/com/mtd/ecom_server/controllers/ProductControllers.java
+++ src/main/java/com/mtd/ecom_server/controllers/ProductControllers.java
@@ -1,7 +1,10 @@
 package com.mtd.ecom_server.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,37 +21,42 @@ import com.mtd.ecom_server.repos.ProductRepo;
 @RestController
 @RequestMapping("/products")
 public class ProductControllers {
+	private static final Logger Log=LoggerFactory.getLogger(ProductControllers.class);
 	@Autowired ProductRepo productRepo ;
 	@GetMapping("/all")
 	public List<product> getAllProducts() {
+		Log.info("fetching products");
 		return productRepo.findAll();
 		
 	}
 @PostMapping("/add")
 public product addProduct(@RequestBody product  newproduct ) {
+	Log.info(" adding Product"+ newproduct);
+
 	return productRepo.save(newproduct);
 }
 
 @DeleteMapping("/product/delete/{id}")
 public String deleteProduct(@PathVariable String id) {
-    product findproduct = productRepo.findById(id).get();
-    if(findproduct != null) {
-        productRepo.deleteById(id);
-        return "Product Deleted " + findproduct.getName();
-    } else {
+    Optional<product> productToDelete = productRepo.findById(id);
+    if(productToDelete.isEmpty()) {
         return "Failed to delete product";
     }
+    productRepo.deleteById(id);
+    Log.info("Product Deleted"+ id);
+    return "product deleted";
+
 }
 
 @PutMapping("/product/edit/{id}")
 public product editProduct(@PathVariable String id, @RequestBody product newproduct) {
-    product findproduct = productRepo.findById(id).get();
-    findproduct.setName(newproduct.getName());
-    findproduct.setDescription(newproduct.getDescription());
-    findproduct.setCategory(newproduct.getCategory());
-    findproduct.setTags(newproduct.getTags()); // comma-separated string
-    findproduct.setPrice(newproduct.getPrice());
-    findproduct.setStock(newproduct.getStock());
-return productRepo.save(findproduct);
+    product productToEdit = productRepo.findById(id).get();
+    productToEdit.setName(newproduct.getName());
+    productToEdit.setDescription(newproduct.getDescription());
+    productToEdit.setCategory(newproduct.getCategory());
+    productToEdit.setTags(newproduct.getTags()); // comma-separated string
+    productToEdit.setPrice(newproduct.getPrice());
+    productToEdit.setStock(newproduct.getStock());
+return productRepo.save(productToEdit);
 }
 }
